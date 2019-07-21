017c00fee6f11cd4ceb6352752edd13fc5dd302e
programminghoch10 <hoch10@kabelbw.de>
2019-07-21 21:42:02 +0200

    fixed nav bar not disappearing after keyboard closed

--- app/src/main/java/com/JJ/weblauncher/MainActivity.java
+++ app/src/main/java/com/JJ/weblauncher/MainActivity.java
@@ -44,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
         myWebView1.setWebViewClient(new WebViewClient());
         webSettings1.setAppCacheEnabled(false);
         webSettings1.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+        myWebView1.setLongClickable(false);
+        myWebView1.setHapticFeedbackEnabled(false);
 
         WebView myWebView2 = findViewById(R.id.webview2);
         myWebView2.loadUrl("about:blank");
@@ -52,6 +60,15 @@ public class MainActivity extends AppCompatActivity {
         myWebView2.setWebViewClient(new WebViewClient());
         webSettings2.setAppCacheEnabled(false);
         webSettings2.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+        myWebView2.setLongClickable(false);
+        myWebView2.setHapticFeedbackEnabled(false);
+
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -61,8 +78,41 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+            }
+        });
+    }
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+            }
+        });
     }
 
     @Override
