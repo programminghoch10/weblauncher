bda677823432ebfae877b685037c0e82f2a2dab6
programminghoch10 <hoch10@kabelbw.de>
2020-01-25 18:57:12 +0100

    reformatted code

--- app/src/main/java/com/JJ/weblauncher/MainActivity.java
+++ app/src/main/java/com/JJ/weblauncher/MainActivity.java
@@ -1,8 +1,6 @@
 package com.JJ.weblauncher;
 
 import android.annotation.SuppressLint;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,17 +13,18 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 	
-	SharedPreferences sharedPreferences;
-	
-	public static int buttoncount = 0;
 	static final String TAG = "MainActivity";
+	public static int buttoncount = 0;
 	public static boolean WiFiCheckerEnabled = false;
 	private static int currentWebView = 0;
+	SharedPreferences sharedPreferences;
 	
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
@@ -48,14 +47,14 @@ public class MainActivity extends AppCompatActivity {
 				findViewById(R.id.webview1),
 				findViewById(R.id.webview2),
 		};
-        for (int i = 0; i < webviews.length; i++) {
-            WebView webView = webviews[i];
-            setupWebView(webView, i + 1);
-        }
+		for (int i = 0; i < webviews.length; i++) {
+			WebView webView = webviews[i];
+			setupWebView(webView, i + 1);
+		}
 		
-        int startMode = sharedPreferences.getInt("StartMode", getResources().getInteger(R.integer.defaultStartMode));
-        switchWebView(startMode);
-  
+		int startMode = sharedPreferences.getInt("StartMode", getResources().getInteger(R.integer.defaultStartMode));
+		switchWebView(startMode);
+		
 		getWindow().getDecorView().setSystemUiVisibility(
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -65,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		
 		final View decorView = getWindow().getDecorView();
-		decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+		decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
 			@Override
 			public void onSystemUiVisibilityChange(int visibility) {
 				if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
@@ -83,10 +82,10 @@ public class MainActivity extends AppCompatActivity {
 	}
 	
 	@Override
-	public void onResume() {
+	protected void onResume() {
 		super.onResume();
 		final View decorView = getWindow().getDecorView();
-		decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+		decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
 			@Override
 			public void onSystemUiVisibilityChange(int visibility) {
 				if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
@@ -115,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
 							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		}
 	}
-	
 	
 	
 	@Override
@@ -152,8 +150,8 @@ public class MainActivity extends AppCompatActivity {
 				WebView webView = currentWebView == 1 ? webView1 : webView2;
 				boolean backEnabled = sharedPreferences.getBoolean("WebView" + currentWebView + "back", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + currentWebView + "back", "bool", getPackageName())));
 				if (webView.canGoBack() && backEnabled) {
-				   webView.goBack();
-				   return true;
+					webView.goBack();
+					return true;
 				}
 				break;
 			default:
@@ -172,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
 		return true;
 	}
 	
-	public void switchWebView(int webviewid) {
+	private void switchWebView(int webviewid) {
 		WebView webView1 = findViewById(R.id.webview1);
 		WebView webView2 = findViewById(R.id.webview2);
 		String webView1URL = sharedPreferences.getString("WebView" + 1 + "URL", getResources().getString(R.string.defaultWebView1URL));
@@ -203,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
 		currentWebView = webviewid;
 	}
 	
-	public void switchToDefaultWebsite(WebView webView, int id) {
+	private void switchToDefaultWebsite(WebView webView, int id) {
 		String url = sharedPreferences.getString("WebView" + id + "URL", getResources().getString(R.string.defaultWebView1URL));
 		webView.loadUrl(url);
 	}
@@ -222,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
 					"haptic",
 					"back",
 			};
-			for (String value: values) {
+			for (String value : values) {
 				String property = getSystemProperty(prefix + getResources().getString(getResources().getIdentifier("WebView" + i + value, "string", getPackageName())));
 				editor.putBoolean("WebView" + i + value, property != null ? Boolean.parseBoolean(property) : getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + i + value, "bool", getPackageName())));
 			}
@@ -242,40 +240,36 @@ public class MainActivity extends AppCompatActivity {
 					new BufferedReader(new InputStreamReader(getPropProcess.getInputStream()));
 			propertyValue = osRes.readLine();
 			osRes.close();
-		} catch (Exception ignored) {} //Do nothing - can't get property value
+		} catch (Exception ignored) {
+		} //Do nothing - can't get property value
 		propertyValue = "".equals(propertyValue) ? null : propertyValue;
 		return propertyValue;
 	}
-    
-    void setupWebView(WebView webView, int id) {
-        //TODO: read values from defaults and preferences manager and depending on id
+	
+	private void setupWebView(WebView webView, int id) {
 		if (id < 1 | id > 2) return;
-        boolean js = sharedPreferences.getBoolean("WebView" + id + "js", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "js", "bool", getPackageName())));
-        boolean cache = sharedPreferences.getBoolean("WebView" + id + "cache", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "cache", "bool", getPackageName())));
-        boolean haptic = sharedPreferences.getBoolean("WebView" + id + "haptic", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "haptic", "bool", getPackageName())));
-        String url = sharedPreferences.getString("WebView" + id + "URL", getResources().getString(getResources().getIdentifier("default" + "WebView" + id + "URL", "string", getPackageName())));
-        setupWebView(webView, url, js, cache, haptic);
-    }
-    
-    @SuppressLint("SetJavaScriptEnabled")
-    private void setupWebView(WebView webView, String url, boolean jsenabled, boolean cacheenabled, boolean hapticfeedbackenabled) {
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(jsenabled);
-        webView.setWebViewClient(new WebViewClient());
-        webSettings.setAppCacheEnabled(cacheenabled);
-        webSettings.setCacheMode(cacheenabled ? WebSettings.LOAD_DEFAULT : WebSettings.LOAD_NO_CACHE);
-        webView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                return true;
-            }
-        });
-        webView.setLongClickable(false);
-        webView.setHapticFeedbackEnabled(hapticfeedbackenabled);
-        webView.loadUrl(url);
-    }
-    
-    void setwebviewpage(WebView webView, String url) {
-        webView.loadUrl(url);
-    }
+		boolean js = sharedPreferences.getBoolean("WebView" + id + "js", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "js", "bool", getPackageName())));
+		boolean cache = sharedPreferences.getBoolean("WebView" + id + "cache", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "cache", "bool", getPackageName())));
+		boolean haptic = sharedPreferences.getBoolean("WebView" + id + "haptic", getResources().getBoolean(getResources().getIdentifier("default" + "WebView" + id + "haptic", "bool", getPackageName())));
+		String url = sharedPreferences.getString("WebView" + id + "URL", getResources().getString(getResources().getIdentifier("default" + "WebView" + id + "URL", "string", getPackageName())));
+		setupWebView(webView, url, js, cache, haptic);
+	}
+	
+	@SuppressLint("SetJavaScriptEnabled")
+	private void setupWebView(WebView webView, String url, boolean jsenabled, boolean cacheenabled, boolean hapticfeedbackenabled) {
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(jsenabled);
+		webView.setWebViewClient(new WebViewClient());
+		webSettings.setAppCacheEnabled(cacheenabled);
+		webSettings.setCacheMode(cacheenabled ? WebSettings.LOAD_DEFAULT : WebSettings.LOAD_NO_CACHE);
+		webView.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				return true;
+			}
+		});
+		webView.setLongClickable(false);
+		webView.setHapticFeedbackEnabled(hapticfeedbackenabled);
+		webView.loadUrl(url);
+	}
 }
