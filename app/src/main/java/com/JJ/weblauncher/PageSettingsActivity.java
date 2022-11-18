83bdb04dd94e06f53c2bdcc4233dede961d92946
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2022-11-18 11:46:03 +0100

    Create Settings activities

---
+++ app/src/main/java/com/JJ/weblauncher/PageSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.JJ.weblauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PageSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
+    }
+}
